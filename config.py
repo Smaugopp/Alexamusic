bd5fb58787f1dde24dba5ebab80634769bb74f67
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-06-18 22:05:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VARC9210/Voidxmusiccc",
+    "https://github.com/Smaugopp/Voidmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
