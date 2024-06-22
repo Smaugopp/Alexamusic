7e44ea786683be21d393bb08da34c3be72a064f5
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-06-22 21:31:44 +0530

    Add files via upload

---
+++ AnonXMusic/mongo/chats_db.py
@@ -0,0 +1,21 @@
+from mirzya.database import db
+
+chatsdb = db.chats
+
+async def get_served_chats() -> list:
+    chats = await chatsdb.find({"chat_id": {"$lt": 0}}).to_list(length=None)
+    return [chat["chat_id"] for chat in chats]
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    return bool(chat)
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_served_chat(chat_id: int):
+    return await chatsdb.delete_one({"chat_id": chat_id})
